b560447b3a8fe593cb621615400ad00198fe145c
Nikhil Haksar <nhaksar@cs.uchicago.edu>
2020-05-18 22:18:46 -0400

    rename dataset, remove values for which we lack CDC data

--- cleaning.R
+++ cleaning.R
@@ -1,14 +1,17 @@
 ## <cleaning.R>
 ## file for loading and cleaning the data
 
-df <- read.xlsx("cntyic.xlsx", startRow=5, colNames=FALSE)
+mls <- read.xlsx("cntyic.xlsx", startRow=5, colNames=FALSE)
 
 ## past County name, all variables are for number of initial unemployment claimants
-names(df) <- c("Year", "State FIPS", "County FIPS", "State_county FIPS",
+names(mls) <- c("Year", "State FIPS", "County FIPS", "State_county FIPS",
                "County name", "Total", "White", "Black", "Hispanic origin",
                "American Indian or Alaskan Native", "Asian or Pacific islander",
                "Race N/A", "Men", "Women", "Gender N/A", "Under30","30-44",
                "45-54","55plus", "Age N/A")
 
 ## there are some entries with NA total entries; remove them
-df <- df[!is.na(df$Total),]
+mls <- mls[!is.na(mls$Total),]
+
+## remove entries before 1999
+mls <- mls[(mls$Year >= 1999),]
