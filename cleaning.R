280cc8acc5d3c08260d3c668df63952f1b81b908
Nikhil Haksar <nhaksar@cs.uchicago.edu>
2020-05-18 21:54:29 -0400

    remove NA values, add comments

--- cleaning.R
+++ cleaning.R
@@ -1,3 +1,6 @@
+## <cleaning.R>
+## file for loading and cleaning the data
+
 df <- read.xlsx("cntyic.xlsx", startRow=5, colNames=FALSE)
 
 ## past County name, all variables are for number of initial unemployment claimants
@@ -6,3 +9,6 @@ names(df) <- c("Year", "State FIPS", "County FIPS", "State_county FIPS",
                "American Indian or Alaskan Native", "Asian or Pacific islander",
                "Race N/A", "Men", "Women", "Gender N/A", "Under30","30-44",
                "45-54","55plus", "Age N/A")
+
+## there are some entries with NA total entries; remove them
+df <- df[!is.na(df$Total),]
