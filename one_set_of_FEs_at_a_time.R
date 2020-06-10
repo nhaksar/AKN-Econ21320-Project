44a71f352e673e51f288048d8caaf46fdde95897
AHKL <ahkandahalf@gmail.com>
2020-06-09 22:50:53 -0500

    one set of FEs at a time + income

--- one_set_of_FEs_at_a_time.R
+++ one_set_of_FEs_at_a_time.R
@@ -94,3 +94,100 @@ stargazer(mod_nolag, mod_1lag, mod_2lag, mod_3lag, mod_4lag, mod_5lag,
           digits=6,
           column.sep.width = "-4pt",
           label="tb:total-fe")
+
+
+##### with income & individual effects only ########
+## total deaths on total layoffs, no lag. within gives FE model
+mod_nolag <- plm(formula = total_deaths ~ Total_layoff + income_pc, 
+                 model="within", data = panel_df)
+
+## total deaths on total layoffs + 1 year lag. FE model
+mod_1lag <- plm(formula = total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1),
+                model="within", data = panel_df)
+
+## total deaths on total layoff + 1,2 year lag. FE model
+mod_2lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1) 
+                + lag(Total_layoff, 2), model="within",
+                data=panel_df)
+
+## total deaths on total layoff + 1,2,3 year lag. FE model
+mod_3lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3),
+                model="within", data=panel_df)
+
+## total deaths on total layoff + 1,2,3,4 year lag. FE model
+mod_4lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4),
+                model="within", data=panel_df)
+## total deaths on total layoff + 1,2,3,4,5 year lag. FE model
+mod_5lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4) + lag(Total_layoff, 5),
+                model="within", data=panel_df)
+
+## generate LaTeX table
+stargazer(mod_nolag, mod_1lag, mod_2lag, mod_3lag, mod_4lag, mod_5lag, 
+          align=TRUE, no.space=TRUE, omit.stat=c("rsq","adj.rsq"),
+          omit="Constant",
+          dep.var.labels = c("Total alcohol and drug deaths"),
+          covariate.labels = c("Total layoffs this year",
+                               "Per Capita Income",
+                               "Total layoffs 1 year ago",
+                               "Total layoffs 2 years ago",
+                               "Total layoffs 3 years ago",
+                               "Total layoffs 4 years ago",
+                               "Total layoffs 5 years ago"),
+          title="Total alcohol and drug deaths regressed on total layoffs with Per Capita Incomecounty fixed effects only",
+          digits=6,
+          column.sep.width = "-4pt",
+          label="tb:total-fe")
+
+
+
+######## with income & time effects only ############
+## total deaths on total layoffs, no lag. within gives FE model
+mod_nolag <- plm(formula = total_deaths ~ Total_layoff + income_pc, 
+                 model="within", effect="time", data = panel_df)
+
+## total deaths on total layoffs + 1 year lag. FE model
+mod_1lag <- plm(formula = total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1),
+                model="within", effect="time", data = panel_df)
+
+## total deaths on total layoff + 1,2 year lag. FE model
+mod_2lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1) 
+                + lag(Total_layoff, 2), model="within", effect="time",
+                data=panel_df)
+
+## total deaths on total layoff + 1,2,3 year lag. FE model
+mod_3lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3),
+                model="within", effect="time",data=panel_df)
+
+## total deaths on total layoff + 1,2,3,4 year lag. FE model
+mod_4lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4),
+                model="within", effect="time", data=panel_df)
+## total deaths on total layoff + 1,2,3,4,5 year lag. FE model
+mod_5lag <- plm(formula=total_deaths ~ Total_layoff + income_pc + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4) + lag(Total_layoff, 5),
+                model="within", effect="time", data=panel_df)
+
+## generate LaTeX table
+stargazer(mod_nolag, mod_1lag, mod_2lag, mod_3lag, mod_4lag, mod_5lag, 
+          align=TRUE, no.space=TRUE, omit.stat=c("rsq","adj.rsq"),
+          omit="Constant",
+          dep.var.labels = c("Total alcohol and drug deaths"),
+          covariate.labels = c("Total layoffs this year",
+                               "Per Capita Income",
+                               "Total layoffs 1 year ago",
+                               "Total layoffs 2 years ago",
+                               "Total layoffs 3 years ago",
+                               "Total layoffs 4 years ago",
+                               "Total layoffs 5 years ago"),
+          title="Total alcohol and drug deaths regressed on total layoffs with Per Capita Income and year fixed effects only",
+          digits=6,
+          column.sep.width = "-4pt",
+          label="tb:total-fe")
