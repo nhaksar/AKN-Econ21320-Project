b288db87a513c04cc21d553068d4eb987b92297a
AHKL <ahkandahalf@gmail.com>
2020-06-10 09:22:00 -0500

    some suicide regressions

--- suicide_analysis.R
+++ suicide_analysis.R
@@ -25,4 +25,151 @@ suicide_df$intentional_and_undetermined[suicide_df$intentional_and_undetermined 
 
 suicide_df$ID <- paste(suicide_df$Year.Code, suicide_df$County.Code, sep = "_")
 
+###### Merging onto main_df ######
+main_df <- merge(main_df, suicide_df, all.x = TRUE, by="ID")
+main_df$Year <- main_df$Year.x
+main_df <- subset(main_df, select = - c(Year.x,Year.y))
+for (i in 9:23){
+  main_df[is.na(main_df[i]),i] = 0
+}
+
+## make some things into factors
+## call names(main_df) to see
+for (i in 1:8){
+  main_df[,i] <- as.factor(main_df[,i])
+}
+main_df$Year <- as.factor(main_df$Year)
+
+
+##### set up panel DF ######
+panel_df <- pdata.frame(main_df, index=c("County_code", "Year"))
+
+
+
+
+##### OLS REGRESSIONS ON TOTAL LAYOFFS #####
+mod_nofe_nolag <- plm(formula = Intentional_self_harm ~ Total_layoff, data=panel_df)
+mod_nofe_1lag <- plm(formula = Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1),
+                     data=panel_df)
+mod_nofe_2lag <- plm(formula = Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + lag(Total_layoff, 2), data=panel_df)
+mod_nofe_3lag <- plm(formula = Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + lag(Total_layoff, 2) + lag(Total_layoff, 3), data=panel_df)
+mod_nofe_4lag <- plm(formula = Intentional_self_harm ~ Total_layoff + 
+                       lag(Total_layoff, 1) + lag(Total_layoff, 2) + 
+                       lag(Total_layoff, 3) + lag(Total_layoff, 4), data=panel_df)
+mod_nofe_5lag <- plm(formula = Intentional_self_harm ~ Total_layoff + 
+                       lag(Total_layoff, 1) + lag(Total_layoff, 2) + 
+                       lag(Total_layoff, 3) + lag(Total_layoff, 4) +
+                       lag(Total_layoff, 5), data=panel_df)
+
+stargazer(mod_nofe_nolag, mod_nofe_1lag, mod_nofe_2lag, mod_nofe_3lag, mod_nofe_4lag, mod_nofe_5lag,
+          align=TRUE, no.space=TRUE, #omit.stat="f",
+          dep.var.labels = c("Total Deaths from Suicide"),
+          covariate.labels = c("Total layoffs this year",
+                               "Total layoffs 1 year ago",
+                               "Total layoffs 2 years ago",
+                               "Total layoffs 3 years ago"),
+          title="Total Deaths from Intentional Self Harm (Suicide) regressed on total layoffs",
+          digits=6,
+          column.sep.width = "-4pt",
+          label="tb:total-nofe")
+
+
+
+##### FE REGRESSIONS ON TOTAL LAYOFFS #####
+# regressions stored as objects. use summary in console to view
+
+## total deaths on total layoffs, no lag. within gives FE model
+mod_nolag <- plm(formula = Intentional_self_harm ~ Total_layoff, 
+                 model="within", effect = "twoways", data = panel_df)
+
+## total deaths on total layoffs + 1 year lag. FE model
+mod_1lag <- plm(formula = Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1),
+                model="within", effect="twoways", data = panel_df)
+
+## total deaths on total layoff + 1,2 year lag. FE model
+mod_2lag <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1) 
+                + lag(Total_layoff, 2), model="within", effect="twoways",
+                data=panel_df)
+
+## total deaths on total layoff + 1,2,3 year lag. FE model
+mod_3lag <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3),
+                model="within", effect="twoways",data=panel_df)
+
+## total deaths on total layoff + 1,2,3,4 year lag. FE model
+mod_4lag <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4),
+                model="within", effect="twoways", data=panel_df)
+## total deaths on total layoff + 1,2,3,4,5 year lag. FE model
+mod_5lag <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                + lag(Total_layoff, 4) + lag(Total_layoff, 5),
+                model="within", effect="twoways", data=panel_df)
+
+## generate LaTeX table
+stargazer(mod_nolag, mod_1lag, mod_2lag, mod_3lag, mod_4lag, mod_5lag, 
+          align=TRUE, no.space=TRUE, omit.stat=c("rsq","adj.rsq"),
+          omit="Constant",
+          dep.var.labels = c("Total Deaths from Suicide"),
+          covariate.labels = c("Total layoffs this year",
+                               "Total layoffs 1 year ago",
+                               "Total layoffs 2 years ago",
+                               "Total layoffs 3 years ago",
+                               "Total layoffs 4 years ago",
+                               "Total layoffs 5 years ago"),
+          title="Total deaths from intentional self harm (suicide) regressed on total layoffs with year and county fixed effects",
+          digits=6,
+          column.sep.width = "-4pt",
+          label="tb:total-fe")
+
+##### REGRESSIONS ON TOTAL LAYOFFS WITH YEAR-STATE FE #####
+## total deaths on total layoffs, no lag. within gives FE model
+mod_nolag_ysfe <- plm(formula = Intentional_self_harm ~ Total_layoff + factor(state_years), 
+                      model="within", data = panel_df)
+
+## total deaths on total layoffs + 1 year lag. FE model
+mod_1lag_ysfe <- plm(formula = Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + factor(state_years),
+                     model="within", effect="twoways", data = panel_df)
+
+## total deaths on total layoff + 1,2 year lag. FE model
+mod_2lag_ysfe <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1) 
+                     + lag(Total_layoff, 2) + factor(state_years), 
+                     model="within", effect="twoways",
+                     data=panel_df)
+
+## total deaths on total layoff + 1,2,3 year lag. FE model
+mod_3lag_ysfe <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + lag(Total_layoff, 2) + lag(Total_layoff, 3) + factor(state_years),
+                     model="within", effect="twoways",data=panel_df)
+
+## total deaths on total layoff + 1,2,3,4 year lag. FE model
+mod_4lag_ysfe <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                     + lag(Total_layoff, 4) + factor(state_years),
+                     model="within", effect="twoways", data=panel_df)
+## total deaths on total layoff + 1,2,3,4,5 year lag. FE model
+mod_5lag_ysfe <- plm(formula=Intentional_self_harm ~ Total_layoff + lag(Total_layoff, 1)
+                     + lag(Total_layoff, 2) + lag(Total_layoff, 3)
+                     + lag(Total_layoff, 4) + lag(Total_layoff, 5) + factor(state_years),
+                     model="within", effect="twoways", data=panel_df)
+
+## generate LaTeX table
+stargazer(mod_nolag_ysfe, mod_1lag_ysfe, mod_2lag_ysfe, mod_3lag_ysfe, mod_4lag_ysfe, 
+          mod_5lag_ysfe, align=TRUE, no.space=TRUE, omit.stat=c("rsq","adj.rsq"),
+          omit="([0-9]{1,2}_[0-9]{4})+",
+          dep.var.labels = c("Total Deaths from Suicide"),
+          covariate.labels = c("Total layoffs this year",
+                               "Total layoffs 1 year ago",
+                               "Total layoffs 2 years ago",
+                               "Total layoffs 3 years ago",
+                               "Total layoffs 4 years ago",
+                               "Total layoffs 5 years ago"),
+          title="Total Deaths from intentional self harm (suicide) regressed on total layoffs with year, county, and state-year fixed effects",
+          digits=6,
+          column.sep.width = "-4pt",
+          label="tb:total-ysfe")
 
