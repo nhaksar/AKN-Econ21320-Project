707e5a6a8ed90916adeb42b7f86541058eb08059
Nikhil Haksar <nhaksar@cs.uchicago.edu>
2020-06-11 21:30:26 -0400

    LASSO updated for more detailed data

--- lasso.R
+++ lasso.R
@@ -5,6 +5,8 @@
 ##### LASSO REGRESSION STUFF #####
 library(glmnet)
 library(caret)
+
+##### LASSO REGRESSIONS FOR ALL
 lasso_df <- panel_df
 
 ## grab lags
@@ -18,48 +20,23 @@ lasso_df$lag5 <- lag(panel_df$Total_layoff, 5)
 lasso_df <- data.frame(as.list(lasso_df))
 
 ## drop NAs
-lasso_df <- na.omit(lasso_df, cols="total_deaths")
-
-## separate train/test samples
-# ind <- sample(1:nrow(lasso_df), 0.7*nrow(lasso_df))
-# train <- lasso_df[ind,]
-# test <- lasso_df[-ind,]
-# 
-# train <- na.omit(train, cols="total_deaths")
-# test <- na.omit(test, cols="total_deaths")
+lasso_df <- na.omit(lasso_df, cols="new_alc_and_drugs")
 
 ## columns of interest
-regressors <- c("Total_layoff", "lag1", "lag2", 
-                "lag3", "lag4", "lag5", 
-                "State_county_FIPS", "Year")
-outcome <- "total_deaths"
 all_vars <- c("Total_layoff", "lag1", "lag2", 
               "lag3", "lag4", "lag5", 
               "State_county_FIPS", "Year",
-              "total_deaths")
+              "new_alc_and_drugs")
 
-## regularize samples
-# preproc <- preProcess(train[,all_vars],
-#                       method=c("center","scale"))
-# train[,all_vars] <- predict(preproc, train[,all_vars])
-# test[,all_vars] <- predict(preproc, test[,all_vars])
+## regularize data
 preproc <- preProcess(lasso_df[,all_vars],
                       method=c("center","scale"))
 lasso_df[,all_vars] <- predict(preproc, lasso_df[,all_vars])
 
-all_effects <- dummyVars(total_deaths ~ ., data = lasso_df[,all_vars])
+all_effects <- dummyVars(new_alc_and_drugs ~ ., data = lasso_df[,all_vars])
 all_effects <- predict(all_effects, lasso_df[,all_vars])
 all_effects <- as.matrix(all_effects)
-# train_effects <- predict(all_effects, train[,all_vars])
-# test_effects <- predict(all_effects, test[,all_vars])
-
-## make data matrices
-# train_effects <- as.matrix(train_effects)
-# test_effects <- as.matrix(test_effects)
-# 
-# train_outcome <- train$total_deaths
-# test_outcome <- test$total_deaths
-all_outcome <- lasso_df$total_deaths
+all_outcome <- lasso_df$new_alc_and_drugs
 
 
 ## use cross-validation to find optimal lambda
